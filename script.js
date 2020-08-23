7780553194d2661d8b1b5d4d2b234d99e0039f2d
Coding <coding@Zakarias-MacBook-Pro.local>
2020-08-23 13:42:28 -0600

    d

--- script.js
+++ script.js
@@ -3,17 +3,10 @@ var mySwiper = new Swiper('.swiper-container', {
     direction: 'horizontal',
     loop: true,
 
-    // If we need pagination
-    pagination: {
-        el: '.swiper-pagination',
-    },
-
     // Navigation arrows
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
     },
-
-    // And if we need scrollbar
 
 })
