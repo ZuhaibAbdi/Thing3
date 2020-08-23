df6114c4d88bdaf53b0af4fc6473a791bd8052d5
Coding <coding@Zakarias-MacBook-Pro.local>
2020-08-23 13:43:47 -0600

    ljkhdljgfdl;hk

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
+import Swiper from 'https://unpkg.com/swiper/swiper-bundle.esm.browser.min.js'
 var mySwiper = new Swiper('.swiper-container', {
     // Optional parameters
     direction: 'horizontal',
     loop: true,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
 
     // Navigation arrows
     navigation: {
@@ -9,4 +15,8 @@ var mySwiper = new Swiper('.swiper-container', {
         prevEl: '.swiper-button-prev',
     },
 
+    // And if we need scrollbar
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
 })
